0e7ebcc1bd11c1fa90b54d6a35706749e083c4a8
Ben Blamey <ben.blamey@springworks.se>
2017-02-13 21:50:46 +0100

    migrate to intellij and gradle

--- src/test/java/com/benblamey/core/classifier/svm/LibSVMTest.java
+++ src/test/java/com/benblamey/core/classifier/svm/LibSVMTest.java
@@ -1,8 +1,10 @@
 package com.benblamey.core.classifier.svm;
 
-import java.util.List;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibSVMTest {
 
